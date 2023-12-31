27534685039a957c52783587cc5d08c695fc09b7
Utsav Jhakhariya <86795590+TheUtsavJhakhariya@users.noreply.github.com>
2023-12-31 16:52:11 -0500

    Update chatpad.js

--- chatpad.js
+++ chatpad.js
@@ -2,7 +2,7 @@ function sendmsg() {
     var inputuser = document.getElementById('msg-input');
     var chats = document.getElementById('chat-log'); 
  
-    if (inputuser.value !== '') { 
+    if (inputuser.value !== '') {
         var newmsg = document.createElement('div');
         newmsg.classList.add('msg'); 
         newmsg.textContent = inputuser.value;
